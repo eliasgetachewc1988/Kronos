e5c468d4abb394aa4d1fb75d2f41da92b6686851
Elias Getachew <47534929+eliasgetachewc1988@users.noreply.github.com>
2026-04-09 04:10:33 +0300

    Update prediction_wo_vol_example.py

--- examples/prediction_wo_vol_example.py
+++ examples/prediction_wo_vol_example.py
@@ -37,7 +37,7 @@ model = Kronos.from_pretrained("NeoQuasar/Kronos-small")
 predictor = KronosPredictor(model, tokenizer, max_context=512)
 
 # 3. Prepare Data
-url = "https://api.twelvedata.com/time_series?apikey=3617d3ff0ca247aeaa7fcb04d0760b66&symbol=XAU/USD&interval=5min&outputsize=2500"
+url = "https://api.twelvedata.com/time_series?apikey=3617d3ff0ca247aeaa7fcb04d0760b66&symbol=XAU/USD&interval=5min&outputsize=2500&timezone=Africa/Nairobi"
 data = requests.get(url).json()
 
 df = pd.DataFrame(data["values"])
